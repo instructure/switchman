df4ffb133bb4f27335631f73eb8319c3c831a9f0
Cody Cutrer <cody@instructure.com>
2013-08-12 16:40:20 -0600

    attribute methods for foreign keys
    closes CNVS-4715
    
    Change-Id: I4c6ba6a9a83890a96b16e3ed8ff1c041355c0f6b
    Reviewed-on: https://gerrit.instructure.com/19537
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- lib/switchman/active_record/attribute_methods.rb
+++ lib/switchman/active_record/attribute_methods.rb
@@ -3,8 +3,15 @@ module Switchman
     module AttributeMethods
       module ClassMethods
         protected
+        def reflection_for_integer_attribute(attr_name)
+          columns_hash[attr_name] && columns_hash[attr_name].type == :integer &&
+              reflections.find { |_, r| r.belongs_to? && r.foreign_key == attr_name }.try(:last)
+        end
+
         def define_method_global_attribute(attr_name)
-          if attr_name == primary_key && integral_id? && self != Shard
+          reflection = reflection_for_integer_attribute(attr_name)
+          if attr_name == primary_key && integral_id? && self != Shard && shard_category != :unsharded ||
+              reflection && (reflection.options[:polymorphic] || reflection.klass.shard_category != :unsharded)
             generated_attribute_methods.module_eval <<-RUBY, __FILE__, __LINE__ + 1
               def __temp__
                 Shard.global_id_for(original_#{attr_name}, shard)
@@ -16,7 +23,9 @@ module Switchman
         end
 
         def define_method_local_attribute(attr_name)
-          if attr_name == primary_key && integral_id? && self != Shard
+          reflection = reflection_for_integer_attribute(attr_name)
+          if attr_name == primary_key && integral_id? && self != Shard && shard_category != :unsharded ||
+              reflection && (reflection.options[:polymorphic] || reflection.klass.shard_category != :unsharded)
             generated_attribute_methods.module_eval <<-RUBY, __FILE__, __LINE__ + 1
               def __temp__
                 Shard.local_id_for(original_#{attr_name}).first
@@ -27,16 +36,40 @@ module Switchman
           end
         end
 
+        def shard_category_code_for_reflection(reflection)
+          if reflection
+            if reflection.options[:polymorphic]
+              # a polymorphic association has to be discovered at runtime. This code ends up being something like
+              # context_type.try(:constantize).try(:shard_category) || :default
+              "#{reflection.options[:foreign_key_type]}.try(:constantize).try(:shard_category) || :default"
+            else
+              # otherwise we can just return a symbol for the statically known type of the association
+              reflection.klass.shard_category.inspect
+            end
+          else
+            shard_category.inspect
+          end
+        end
+
         def define_method_original_attribute(attr_name)
-          if attr_name == primary_key && integral_id? && self != Shard
+          reflection = reflection_for_integer_attribute(attr_name)
+          if attr_name == primary_key && integral_id? && self != Shard && shard_category != :unsharded ||
+              reflection && (reflection.options[:polymorphic] || reflection.klass.shard_category != :unsharded)
             generated_attribute_methods.module_eval <<-RUBY, __FILE__, __LINE__ + 1
               # rename the original method to original_
               alias_method 'original_#{attr_name}', '#{attr_name}'
               # and replace with one that transposes the id
               def __temp__
-                Shard.relative_id_for(local_#{attr_name}, shard, Shard.current(self.class.shard_category))
+                Shard.relative_id_for(original_#{attr_name}, shard, Shard.current(#{shard_category_code_for_reflection(reflection)}))
               end
               alias_method '#{attr_name}', :__temp__
+              undef_method :__temp__
+
+              alias_method 'original_#{attr_name}=', '#{attr_name}='
+              def __temp__(new_value)
+                self.original_#{attr_name} = Shard.relative_id_for(new_value, Shard.current(#{shard_category_code_for_reflection(reflection)}), shard)
+              end
+              alias_method '#{attr_name}=', :__temp__
               undef_method :__temp__
             RUBY
           end
--- spec/lib/active_record/attribute_methods_spec.rb
+++ spec/lib/active_record/attribute_methods_spec.rb
@@ -18,6 +18,90 @@ module Switchman
             user.global_id.should > Shard::IDS_PER_SHARD
           end
         end
+
+        it "should return foreign keys relative to the current shard" do
+          appendage = Appendage.create!
+
+          # bypass the setter; we're going to test it in just a minute
+
+          # local id, should stay local
+          appendage.original_user_id = 6
+          appendage.user_id.should == 6
+
+          # (incorrect) self referencing global id; should come out as local
+          appendage.original_user_id = Shard.current.global_id_for(6)
+          appendage.user_id.should == 6
+
+          # global id referencing another shard; should come out unscathed
+          appendage.original_user_id = @shard1.global_id_for(6)
+          appendage.user_id.should == @shard1.global_id_for(6)
+
+          @shard1.activate do
+            # local id in another shard, should be global in this shard
+            appendage.original_user_id = 6
+            appendage.user_id.should == Shard.default.global_id_for(6)
+
+            # (incorrect) self referencing global id; should come out as global in this shard
+            appendage.original_user_id = Shard.default.global_id_for(6)
+            appendage.user_id.should == Shard.default.global_id_for(6)
+
+            # global id referencing this shard; should come out as a local id in this shard
+            appendage.original_user_id = @shard1.global_id_for(6)
+            appendage.user_id.should == 6
+
+            # global id from an unrelated shard; should stay global
+            appendage.original_user_id = @shard2.global_id_for(6)
+            appendage.user_id.should == @shard2.global_id_for(6)
+          end
+
+          # now that we trust the getters, try the setters
+
+          # local stays local
+          appendage.user_id = 6
+          appendage.original_user_id.should == 6
+          appendage.user_id = '6'
+          appendage.original_user_id.should == 6
+
+          # (incorrect) global id to this shard, should become local
+          appendage.user_id = Shard.current.global_id_for(6)
+          appendage.original_user_id.should == 6
+          appendage.user_id = Shard.current.global_id_for(6).to_s
+          appendage.original_user_id.should == 6
+
+          # global id from another shard, should stay global
+          appendage.user_id = @shard1.global_id_for(6)
+          appendage.original_user_id.should == @shard1.global_id_for(6)
+          appendage.local_user_id.should == 6
+          appendage.user_id = @shard1.global_id_for(6).to_s
+          appendage.original_user_id.should == @shard1.global_id_for(6)
+          appendage.local_user_id.should == 6
+
+          @shard1.activate do
+            # local to this shard becomes global
+            appendage.user_id = 6
+            appendage.original_user_id.should == @shard1.global_id_for(6)
+            appendage.user_id = '6'
+            appendage.original_user_id.should == @shard1.global_id_for(6)
+
+            # global id from original shard, should become local
+            appendage.user_id = Shard.default.global_id_for(6)
+            appendage.original_user_id.should == 6
+            appendage.user_id = Shard.default.global_id_for(6).to_s
+            appendage.original_user_id.should == 6
+
+            # global id from this shard, should stay global
+            appendage.user_id = Shard.current.global_id_for(6)
+            appendage.original_user_id.should == @shard1.global_id_for(6)
+            appendage.user_id = Shard.current.global_id_for(6).to_s
+            appendage.original_user_id.should == @shard1.global_id_for(6)
+
+            # global id from unrelated shard, should stay global
+            appendage.user_id = @shard2.global_id_for(6)
+            appendage.original_user_id.should == @shard2.global_id_for(6)
+            appendage.user_id = @shard2.global_id_for(6).to_s
+            appendage.original_user_id.should == @shard2.global_id_for(6)
+          end
+        end
       end
     end
   end

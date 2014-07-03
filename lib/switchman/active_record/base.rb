26bbc59fd16f98d5bda0a0420f14955598c5fe76
James Williams <jamesw@instructure.com>
2014-07-03 14:06:58 -0600

    make shard scope around saves implicit
    prevents problems with explicit shard source value
    in save callbacks
    
    fixes #CNVS-13883
    
    Change-Id: I5ca53a7391f5f0ba49f3b0b653cab2c8f4659503
    Reviewed-on: https://gerrit.instructure.com/37282
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- lib/switchman/active_record/base.rb
+++ lib/switchman/active_record/base.rb
@@ -82,16 +82,16 @@ module Switchman
 
       def save(*args)
         @shard_set_in_stone = true
-        self.class.shard(shard).scoping { super }
+        self.class.shard(shard, :implicit).scoping { super }
       end
 
       def save!(*args)
         @shard_set_in_stone = true
-        self.class.shard(shard).scoping { super }
+        self.class.shard(shard, :implicit).scoping { super }
       end
 
       def destroy
-        self.class.shard(shard).scoping { super }
+        self.class.shard(shard, :implicit).scoping { super }
       end
 
       def clone

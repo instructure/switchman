bb6835d8947db860eb8e7c0efdeab9b7b2437cdd
Cody Cutrer <cody@instructure.com>
2017-06-13 11:03:02 -0600

    include, not prepend, ActiveRecord::CollectionProxy
    we don't actually care about the order for initialize, and this is
    necessary to avoid https://bugs.ruby-lang.org/issues/13656.
    
    also add a detection for similar bugs in the future, so that we'll
    raise an exception instead of going into an infinite loop
    
    Change-Id: Ia2e4fef6523fcd00626b64a161df1d7ae3757dde
    Reviewed-on: https://gerrit.instructure.com/115264
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins

--- lib/switchman/call_super.rb
+++ lib/switchman/call_super.rb
@@ -2,8 +2,10 @@ module Switchman
   module CallSuper
     def super_method_above(method_name, above_module)
       method = method(method_name)
+      last_owner = method.owner
       while method.owner != above_module
         method = method.super_method
+        raise "Could not find super method ``#{method_name}' for #{self.class}" if method.owner == last_owner
       end
       method.super_method
     end

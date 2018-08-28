3b576d028befc1d725b16bac8de227fd3aa9a2fd
Cody Cutrer <cody@instructure.com>
2018-08-28 12:44:15 -0600

    improve performance of querying the default shard
    also ensure that there's only one default shard (on postgres)
    
    Change-Id: I3d746e8d281673abab53b89bd5b47eaca33d9300
    Reviewed-on: https://gerrit.instructure.com/162356
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

---
+++ db/migrate/20180828183945_add_default_shard_index.rb
@@ -0,0 +1,13 @@
+class AddDefaultShardIndex < ActiveRecord::Migration[4.2]
+  def change
+    Switchman::Shard.where(default: nil).update_all(default: false)
+    change_column_default :switchman_shards, :default, false
+    change_column_null :switchman_shards, :default, false
+    options = if connection.adapter_name == 'PostgreSQL'
+                { unique: true, where: "\"default\"" }
+              else
+                {}
+              end
+    add_index :switchman_shards, :default, options
+  end
+end

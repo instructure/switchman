d02cb56a4ddaced2b80183b6c6ac71d6f5b3762d
Cody Cutrer <cody@instructure.com>
2018-08-28 13:44:06 -0600

    clear shard cache before reloading default shard
    Change-Id: I232356f506534ccfc913a39de4e8679c8f3e56aa
    Reviewed-on: https://gerrit.instructure.com/162384
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins

--- lib/switchman/test_helper.rb
+++ lib/switchman/test_helper.rb
@@ -3,6 +3,7 @@ module Switchman
     class << self
       def recreate_persistent_test_shards(dont_create: false)
         # recreate the default shard (it got buhleted)
+        Switchman.cache.clear
         if Shard.default(true).is_a?(DefaultShard)
           begin
             Shard.create!(default: true)

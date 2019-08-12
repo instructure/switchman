f729318b3d3eba6e7fd2aee226005dc1ba56ce55
Cody Cutrer <cody@instructure.com>
2019-08-12 10:07:13 -0600

    handle RedisCacheStore#clear with namespacing
    Change-Id: I79c449e9730ae5eee7cc2fdd11bb7c294e074844
    Reviewed-on: https://gerrit.instructure.com/204664
    Reviewed-by: Derek Bender <djbender@instructure.com>
    Tested-by: Jenkins
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- lib/switchman/active_support/cache.rb
+++ lib/switchman/active_support/cache.rb
@@ -4,8 +4,24 @@ module Switchman
       module ClassMethods
         def lookup_store(*store_options)
           store = super
+          # can't use defined?, because it's a _ruby_ autoloaded constant,
+          # so just checking that will cause it to get required
+          if store.class.name == "ActiveSupport::Cache::RedisCacheStore" && !::ActiveSupport::Cache::RedisCacheStore.ancestors.include?(RedisCacheStore)
+            ::ActiveSupport::Cache::RedisCacheStore.prepend(RedisCacheStore)
+          end
           store.options[:namespace] ||= lambda { Shard.current.default? ? nil : "shard_#{Shard.current.id}" }
           store
+        end
+      end
+
+      module RedisCacheStore
+        def clear(options = {})
+          # RedisCacheStore tries to be smart and only clear the cache under your namespace, if you have one set
+          # unfortunately, it uses the keys command, which is extraordinarily inefficient in a large redis instance
+          # fortunately, we can assume we control the entire instance, because we set up the namespacing, so just
+          # always unset it temporarily for clear calls
+          options[:namespace] = nil
+          super
         end
       end
     end

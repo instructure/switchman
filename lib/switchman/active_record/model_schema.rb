7477759ab2d93757f154b9f7611448cddfbb7a56
Cody Cutrer <cody@instructure.com>
2019-09-05 15:12:36 -0600

    cache quoted table name based on the current shard for the correct category
    Change-Id: I3bda1ec55c0f25065b04968056167abe5981bb26
    Reviewed-on: https://gerrit.instructure.com/208382
    Tested-by: Jenkins
    Reviewed-by: James Williams <jamesw@instructure.com>
    QA-Review: James Williams <jamesw@instructure.com>
    Product-Review: James Williams <jamesw@instructure.com>

--- lib/switchman/active_record/model_schema.rb
+++ lib/switchman/active_record/model_schema.rb
@@ -4,7 +4,7 @@ module Switchman
       module ClassMethods
         def quoted_table_name
           @quoted_table_name ||= {}
-          @quoted_table_name[Shard.current.id] ||= connection.quote_table_name(table_name)
+          @quoted_table_name[Shard.current(shard_category).id] ||= connection.quote_table_name(table_name)
         end
       end
     end

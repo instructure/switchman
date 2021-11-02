1777bf268d2845d782dd21476bd8da9f6a50dfea
Jacob Burroughs <jburroughs@instructure.com>
2021-11-02 11:39:08 -0500

    Make AddTimestampsToShards idempotent
    Change-Id: I4b4913ea14592185899115ce4732cefd6ca5c0f9
    Reviewed-on: https://gerrit.instructure.com/c/switchman/+/277272
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20180828192111_add_timestamps_to_shards.rb
+++ db/migrate/20180828192111_add_timestamps_to_shards.rb
@@ -4,7 +4,7 @@ class AddTimestampsToShards < ActiveRecord::Migration[4.2]
   disable_ddl_transaction!
 
   def change
-    add_timestamps :switchman_shards, null: true
+    add_timestamps :switchman_shards, null: true, if_not_exists: true
     now = Time.now.utc
     Switchman::Shard.update_all(updated_at: now, created_at: now) if Switchman::Shard.current.default?
     change_column_null :switchman_shards, :updated_at, false

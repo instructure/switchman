1400341bdc9f0539ce510fa45386f6e7cac3d436
Alex Slaughter <aslaughter@instructure.com>
2022-04-18 12:25:18 -0700

    add tests around load order and rails cache namespace
    Change-Id: I89be468f83918b550d6ae364d954d5837cad4a24
    Reviewed-on: https://gerrit.instructure.com/c/switchman/+/289843
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Alex Slaughter <aslaughter@instructure.com>
    Product-Review: Alex Slaughter <aslaughter@instructure.com>

---
+++ spec/lib/engine_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+module Switchman
+  describe Engine do
+    include RSpecHelper
+
+    it 'registers initializers in the correct order' do
+      order = ::Rails.application.initializers.map(&:name)
+
+      # These are part of Rail::Railtie - we never want to supersede these
+      indexes = []
+      indexes.push(order.index(:load_environment_hook))
+      indexes.push(order.index(:load_active_support))
+      indexes.push(order.index(:set_eager_load))
+      indexes.push(order.index(:initialize_logger))
+      indexes.push(order.index(:initialize_cache))
+      indexes.push(order.index(:initialize_dependency_mechanism))
+      indexes.push(order.index(:bootstrap_hook))
+      indexes.push(order.index(:set_secrets_root))
+
+      indexes_max = indexes.max
+
+      # Our initializers will and should always be after the second :set_autoload_paths
+      # The first one is the application switchman is installed in, the second is switchman itself
+      # followed by any other engines registered
+      lower_bound = order.each_index.select { |i| order[i] == :set_autoload_paths }.second
+      sm_arp = order.index('switchman.active_record_patch')
+      sm_ep = order.index('switchman.error_patch')
+      sm_ic = order.index('switchman.initialize_cache')
+
+      expect(indexes_max).to be < lower_bound
+
+      expect(lower_bound).to be < sm_arp # switchman.active_record_patch is first
+      expect(sm_ep).to eq(sm_arp + 1) # switchman.error_patch is second
+      expect(sm_ic).to eq(sm_ep + 1) # switchman.initialize_cache is third
+    end
+  end
+end
--- spec/lib/rails_spec.rb
+++ spec/lib/rails_spec.rb
@@ -12,6 +12,17 @@ module Switchman
 
       expect(@shard1.activate { ::Rails.cache }).to eq(@shard2.activate { ::Rails.cache })
 
+      shard_1_namespace = @shard1.activate do
+        ::Rails.cache.options[:namespace].call
+      end
+
+      shard_2_namespace = @shard2.activate do
+        ::Rails.cache.options[:namespace].call
+      end
+
+      expect(shard_1_namespace).to eq("shard_#{@shard1.id}")
+      expect(shard_2_namespace).to eq("shard_#{@shard2.id}")
+
       from1 = @shard1.activate { ::Rails.cache.fetch('key') { 1 } }
       expect(from1).to eq 1
       from2 = @shard2.activate do

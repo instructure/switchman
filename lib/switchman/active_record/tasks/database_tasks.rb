df2a5e02af406e93c8b35e318f5f78c03b13c706
Cody Cutrer <cody@instructure.com>
2023-06-21 12:07:05 -0600

    disable Rails 7's checks for multi-db in rake tasks
    Change-Id: I15dfd90b65e4d72e050f823de2067628a0286427
    Reviewed-on: https://gerrit.instructure.com/c/switchman/+/321039
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- lib/switchman/active_record/tasks/database_tasks.rb
+++ lib/switchman/active_record/tasks/database_tasks.rb
@@ -10,6 +10,11 @@ module Switchman
           Switchman.cache.delete("default_shard")
           Shard.default(reload: true)
         end
+
+        def raise_for_multi_db(*)
+          # ignore; Switchman doesn't use namespaced tasks for multiple shards; it uses
+          # environment variables to filter which shards you want to target
+        end
       end
     end
   end
